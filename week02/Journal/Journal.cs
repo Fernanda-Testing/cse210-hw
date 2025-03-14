ef6a52aa20b3c57b580b2655e8ff63c1d940555b
Fernanda-Testing <mfsouzafaria@byupathway.edu>
2025-03-14 00:13:27 -0300

    W02 Commit Team Activity

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,25 @@
+using System.Runtime.CompilerServices;
+
+public class Journal 
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile()
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+
+    }
+}
