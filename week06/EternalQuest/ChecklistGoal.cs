5dfa9c4f1962376e0c4891f16556aa735cb1e9ef
Fernanda-Testing <mfsouzafaria@byupathway.edu>
2025-04-12 17:15:11 -0300

    Commit - W06 Team Activity: Eternal Quest Design

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics.Contracts;
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, string points, int target, int bonus) : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = 0;
+    }
+    public override void RecordEvent()
+    {
+        
+    }
+
+    public override bool IsComplete()
+    {
+        return true;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+    public override string GetDetailsString()
+    {
+        return "";
+    }
+}
